d1d58e10f052d1ebe442bd0ea52f104604cb4dd3
zakir <80246097+zakir-code@users.noreply.github.com>
2024-09-11 18:03:32 +0800

    lint

--- testing/simapp/app.go
+++ testing/simapp/app.go
@@ -358,6 +358,7 @@ func NewSimApp(
 	app.BankKeeper = bankkeeper.NewBaseKeeper(
 		appCodec,
 		runtime.NewKVStoreService(keys[banktypes.StoreKey]),
+		keys[banktypes.StoreKey],
 		app.AccountKeeper,
 		BlockedAddresses(),
 		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
